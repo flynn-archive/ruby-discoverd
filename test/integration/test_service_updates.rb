8a5d7333d264f31d3e2db37cbe046cf18709fddb
Lewis Marshall <lewis@lmars.net>
2014-02-22 15:23:45 +0000

    Implement Discover::Service#each_update
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

---
+++ test/integration/test_service_updates.rb
@@ -0,0 +1,44 @@
+require "test_helper"
+
+class TestServiceUpdates < DiscoverIntegrationTest
+  class TestServiceWatcher
+    include Celluloid
+
+    attr_reader :updates
+
+    def initialize(service)
+      @service = service
+      @updates = []
+      async.watch
+    end
+
+    def watch
+      @service.each_update do |update|
+        @updates.push update
+      end
+    end
+  end
+
+  def test_registration_triggers_updates
+    name = "foo"
+    ip   = "127.0.0.1"
+
+    service = @client.service(name)
+    watcher = TestServiceWatcher.new(service)
+
+    @client.register name, 1111, ip
+    sleep(0.2)
+    assert_equal 1, watcher.updates.size
+    assert_equal "#{ip}:1111", watcher.updates.last.address
+
+    @client.register name, 2222, ip
+    sleep(0.2)
+    assert_equal 2, watcher.updates.size
+    assert_equal "#{ip}:2222", watcher.updates.last.address
+
+    @client.register name, 1111, ip, { "foo" => "bar" }
+    sleep(0.2)
+    assert_equal 3, watcher.updates.size
+    assert_equal "#{ip}:1111", watcher.updates.last.address
+  end
+end
---
+++ test/support/discover_integration_test.rb
@@ -0,0 +1,21 @@
+require "securerandom"
+
+class DiscoverIntegrationTest < Minitest::Test
+  def setup
+    etcd_name = SecureRandom.hex
+    @etcd_pid = spawn("etcd -name #{etcd_name}", [:out, :err] => "/dev/null")
+    sleep(0.2)
+
+    @discoverd_pid = spawn("discoverd", [:out, :err] => "/dev/null")
+    sleep(0.2)
+
+    @client = Discover::Client.new
+  end
+
+  def teardown
+    sleep(0.2)
+    Process.kill("TERM", @discoverd_pid)
+    Process.kill("TERM", @etcd_pid)
+    Process.waitall
+  end
+end
